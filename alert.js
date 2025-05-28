4fa81cc0eb36e97a03f859c8d76d16c074c324cf
Charles <2236001477@utna.edu.mx>
2025-05-27 18:02:24 -0600

    Se agrego codigo again

---
+++ alert.js
@@ -0,0 +1,7 @@
+// Definir la función que muestra una alerta
+function mostrarAlerta() {
+    alert("¡Hola! Esta es una alerta desde JavaScript.");
+}
+
+// Llamar la función para que se ejecute
+mostrarAlerta();
